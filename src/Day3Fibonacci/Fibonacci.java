50c8140dbe0983e595a437a36e5f54b2cf014b5f
Dkrishnamohanpanda <dkrishnamohanonline@gmail.com>
2024-10-15 19:33:15 +0530

    Leetcode 503

---
+++ src/Day3Fibonacci/Fibonacci.java
@@ -0,0 +1,18 @@
+package Day3Fibonacci;
+
+public class Fibonacci {
+    static int fib(int n){
+        if( n <=1){
+            return n;
+        }
+            return fib(n-1) + fib(n-2);
+
+    }
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println("Fibonacci series is");
+        for(int i = 0;i<n;i++){
+            System.out.println(fib(i) + " ");
+        }
+    }
+}
