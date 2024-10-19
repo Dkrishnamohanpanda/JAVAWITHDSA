e6b75b99cb109ca785be27867077aeebac00094b
Dkrishnamohanpanda <dkrishnamohanonline@gmail.com>
2024-10-19 07:30:36 +0530

    Control Statement And HackerRank Problem

---
+++ src/controlEx_17_10_24/Ex7_17_10_24_Hackerrank.java
@@ -0,0 +1,26 @@
+package controlEx_17_10_24;
+
+import java.util.Scanner;
+
+public class Ex7_17_10_24_Hackerrank {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the marks");
+        int num = sc.nextInt();
+        if(num >=90 && num <=100){
+            System.out.println("grade A");
+        } else if (num >=80 && num<90) {
+            System.out.println("grade B ");
+        } else if (num >=70 && num <80) {
+            System.out.println("grade C");
+        } else if (num >=60 && num <70) {
+            System.out.println("grade D");
+        }else if (num <=0 && num > 70) {
+            System.out.println("Are you LOl");
+            
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
